66258dd5287efc27bff4e81ec201951df52c3b80
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2025-11-12 13:00:04 +0100

    added useState and useEffect to manage the fetch of actresses

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import axios from 'axios'
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [actress, setActress] = useState([])
+  //console.log(actress);
   const actressesApiUrl = 'https://lanciweb.github.io/demo/api/actresses/'
 
-  axios.get(actressesApiUrl)
-    .then(response => {
-      const listActresses = response.data
-      console.log(listActresses);
+  function getActresses() {
+    axios.get(actressesApiUrl)
+      .then(response => {
+        const listActresses = response.data
+        //console.log(listActresses);
+        setActress(listActresses)
 
-    })
-    .catch(error => {
-      console.error('Errore nella chiamata API:', error);
-    });
+      })
+      .catch(error => {
+        console.error('Errore nella chiamata API:', error);
+      });
+  }
+
+  useEffect(() => {
+    getActresses();
+  }, []);
 
   return (
     <>
