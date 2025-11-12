80bdbc4f55e4ce72bd5ae50d96a41c0bcd7c304a
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2025-11-12 12:38:56 +0100

    Milestone 1: Ajax call made and result printed in console

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,16 @@ import axios from 'axios'
 function App() {
   const actressesApiUrl = 'https://lanciweb.github.io/demo/api/actresses/'
 
+  axios.get(actressesApiUrl)
+    .then(response => {
+      const listActresses = response.data
+      console.log(listActresses);
+
+    })
+    .catch(error => {
+      console.error('Errore nella chiamata API:', error);
+    });
+
   return (
     <>
 
